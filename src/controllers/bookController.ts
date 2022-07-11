f90c30146ac21a08d3a55232eedf8ccbc589bc6f
Techatonic <techatonicgames@gmail.com>
2022-07-11 12:38:02 +0100

    getBooksAlphabetically and getCheckedOutBooks endpoints implemented

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -10,7 +10,13 @@ class BookController {
         this.router = Router();
         this.router.get('/:id', this.getBook.bind(this));
         this.router.get('/', this.getAllBooks.bind(this));
-
+        this.router.get(
+            '/alphabetical',
+            this.getBooksAlphabetically.bind(this),
+        );
+        this.router.get('/checkedOut', this.getCheckedOutBooks.bind(this));
+        this.router.get('title/:title', this.getBooksByTitle.bind(this));
+        this.router.get('author/:author', this.getBooksByTitle.bind(this));
         this.router.post('/', this.createBook.bind(this));
     }
 
@@ -49,6 +55,85 @@ class BookController {
     }
 
     createBook(req: Request, res: Response) {
+        // TODO: implement functionality
+        return res.status(500).json({
+            error: 'server_error',
+            error_description: 'Endpoint not implemented yet.',
+        });
+    }
+
+    getCheckedOutBooks(req: Request, res: Response) {
+        // TODO: implement functionality
+        const queryStatement =
+            'SELECT * FROM Books JOIN User_Books on Books.id = User_Books.book_id_val JOIN Users on User_Books.user_id_val = Users.id WHERE User_Books.returned_date is NULL;';
+        const data = {};
+        const result = executeStatement(queryStatement)
+            .then((rows: Array<Record<string, any>>) => {
+                console.log(rows);
+
+                rows.forEach((row) => {
+                    data[row.id] = new Book(
+                        row.id,
+                        row.title,
+                        row.isbn,
+                        row.copies_owned,
+                    );
+                });
+
+                return res.status(200).json({
+                    status: 200,
+                    data: data,
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    getBooksAlphabetically(req: Request, res: Response) {
+        // TODO: implement functionality
+        const queryStatement = 'SELECT * FROM Books ORDER BY Title ASC';
+        const data = {};
+        const result = executeStatement(queryStatement)
+            .then((rows: Array<Record<string, any>>) => {
+                console.log(rows);
+
+                rows.forEach((row) => {
+                    data[row.id] = new Book(
+                        row.id,
+                        row.title,
+                        row.isbn,
+                        row.copies_owned,
+                    );
+                });
+
+                return res.status(200).json({
+                    status: 200,
+                    data: data,
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    getBooksByTitle(req: Request, res: Response) {
+        // TODO: implement functionality
+        return res.status(500).json({
+            error: 'server_error',
+            error_description: 'Endpoint not implemented yet.',
+        });
+    }
+
+    getBooksByAuthor(req: Request, res: Response) {
+        // TODO: implement functionality
+        return res.status(500).json({
+            error: 'server_error',
+            error_description: 'Endpoint not implemented yet.',
+        });
+    }
+
+    getCopiesForBook(req: Request, res: Response) {
         // TODO: implement functionality
         return res.status(500).json({
             error: 'server_error',
